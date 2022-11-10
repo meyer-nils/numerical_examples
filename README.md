2e87c6ea7948f07e5cc8227ab01226eaf3bdd81f
Nils Meyer <nils.meyer@kit.edu>
2022-11-10 19:17:49 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Plate hole example
+
+Application of several numerical algortihms and tools to the elastic prolem of a simple plate with a hole.
---
+++ global_constants.py
@@ -0,0 +1,14 @@
+# Quadratic plate length
+
+L = 1.0
+# Circle radius and location
+X0 = 0.5
+Y0 = 0.5
+R = 0.25
+
+# Applied strain at right boundary
+EPS0 = 0.01
+
+# Lame constants
+MU = 1
+LBD = 2
