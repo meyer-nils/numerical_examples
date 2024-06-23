2b8086174af5edf3b23e544aeb88b60a9c787278
Nils Meyer <nils.meyer@uni-a.de>
2024-06-23 16:17:59 +0200

    Clean up plate hole 2d example.

--- plate_hole_2d/global_constants.py
+++ plate_hole_2d/global_constants.py
@@ -1,14 +1,9 @@
-# Quadratic plate length
+# Dimensions
 L = 1.0
-
-# Circle radius
 R = 0.5
 
 # Applied strain at right boundary
 EPS0 = 0.1
-
-# Applied body force field
-B0 = 0.0
 
 # Lame constants
 MU = 1
